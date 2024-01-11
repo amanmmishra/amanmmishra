e2eaa72351fb2338c58c0a1bf914e9d1a5f1d76b
Aman Kumar Mishra <100419712+amanmmishra@users.noreply.github.com>
2024-01-11 10:22:52 +0530

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,22 @@
+def building (){
+                echo 'building the app'
+                echo "BR_Name: ${BUILD_VERSION}"
+                echo "Status: ${YN}"
+                echo "Color: ${COLOR}"
+}
+
+def testing (){
+               echo 'testing the app'
+                echo "BR_Name: ${BUILD_VERSION}"
+                echo "Status: ${YN}"
+                echo "Color: ${COLOR}"
+}
+
+def deploying (){
+                echo 'deploying the app'
+                echo "BR_Name: ${BUILD_VERSION}"
+                echo "Status: ${YN}"
+                echo "Color: ${COLOR}"
+}
+
+return this
